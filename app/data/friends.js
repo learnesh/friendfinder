536b74c1b2fcdb41eea98a70c96dc820c58349bd
learnesh <32178001+learnesh@users.noreply.github.com>
2018-03-09 21:08:50 -0600

    Create friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+//array that holds all the 'friends' that have taken the survey
+var friendList = [
+  {
+    name: 'Sarah Learner,'
+    profilePic: 'https://avatars3.githubusercontent.com/u/32178001?s=400&u=7ab64a8c7c3c76b80f4bf875c6e08880ff995385&v=4'
+    scores: [
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+  }
+
+module.exports = friendList;
